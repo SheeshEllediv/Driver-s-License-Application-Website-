5c170cd0f4c3105ce9ca0c5064d14d14f17b997e
Videlle Pepito <videllepepito15@gmail.com>
2023-06-18 17:01:04 +0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("showTermsBtn").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "flex";
+  });
+  
+  document.getElementById("close").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+  });
